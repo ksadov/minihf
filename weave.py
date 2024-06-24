c2202c4e6111eaafabcbd27fb4241fc67cff323b
K Sadov <ksadov@gmail.com>
2024-06-24 16:28:20 -0500

    factor out evaluator and generator into their own classes

--- weave.py
+++ weave.py
@@ -3,7 +3,6 @@
 """Samples from a language model using Weave tree search."""
 
 import argparse
-import json
 from functools import partial
 import heapq
 from itertools import chain
@@ -11,15 +10,11 @@ import math
 from operator import attrgetter
 import os
 import random
-import asyncio
 
-import requests
 from rich import print as rprint
 from rich.traceback import install
-import torch
-from tqdm import tqdm
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.generation.streamers import BaseStreamer
+
+from infer import make_gen_eval_fns
 
 from utils import load_config
 
@@ -56,130 +51,6 @@ def gumbelvariate(loc=0.0, scale=1.0):
     return loc - scale * math.log(random.expovariate(1))
 
 
-class ProgressBarStreamer(BaseStreamer):
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.kwargs = kwargs
-        self.kwargs.setdefault("unit", "tok")
-        self.next_tokens_are_prompt = True
-        self.pbar = None
-
-    def __enter__(self):
-        self.pbar = tqdm(**self.kwargs)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.pbar.close()
-
-    def put(self, value):
-        if not self.next_tokens_are_prompt:
-            self.pbar.update(value.numel())
-        self.next_tokens_are_prompt = False
-
-    def end(self):
-        self.next_tokens_are_prompt = True
-
-
-def load_generator(model_name="mistralai/Mistral-7B-v0.1", load_dtype="int8"):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.truncation_side = "left"
-    tokenizer.padding_side = "left"
-    tokenizer.pad_token = tokenizer.eos_token
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        device_map="auto",
-        load_in_4bit=load_dtype == "int4",
-        load_in_8bit=load_dtype == "int8",
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
-    )
-    return tokenizer, model
-
-
-def load_evaluator(model_name="jdpressman/minihf_evaluator_mistral_7b_v0.1", load_dtype="int4"):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.truncation_side = "left"
-    tokenizer.padding_side = "left"
-    tokenizer.pad_token = tokenizer.eos_token
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        device_map="auto",
-        load_in_4bit=load_dtype == "int4",
-        load_in_8bit=load_dtype == "int8",
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
-    )
-    return tokenizer, model
-
-
-def get_scores_from_logits(logits, pos_tokens, neg_tokens, alpha=float("-inf")):
-    logits = logits[:, -1, :].float()
-    logits = torch.nn.functional.log_softmax(logits, dim=-1)
-    pos_logits = torch.logsumexp(logits[:, pos_tokens], dim=-1)
-    neg_logits = torch.logsumexp(logits[:, neg_tokens], dim=-1)
-    alpha = logits.new_tensor(alpha)
-    return torch.logaddexp(pos_logits, alpha) - torch.logaddexp(neg_logits, alpha)
-
-
-get_scores_from_logits_gpt2 = partial(
-    get_scores_from_logits,
-    pos_tokens=[3363, 3763, 5297, 8505, 21560, 43335],
-    neg_tokens=[645, 1400, 2949, 3919, 8005, 15285],
-)
-
-
-get_scores_from_logits_neox = partial(
-    get_scores_from_logits,
-    pos_tokens=[4374, 4754, 6279, 9820, 22487, 24239],
-    neg_tokens=[642, 1621, 2302, 2369, 7651, 7716],
-)
-
-
-get_scores_from_logits_llama = partial(
-    get_scores_from_logits,
-    pos_tokens=[3582, 3869, 4874, 8241, 21143, 22483],
-    neg_tokens=[694, 1217, 1939, 3782, 6632, 11698],
-)
-
-
-get_scores_from_logits_openllama = partial(
-    get_scores_from_logits,
-    pos_tokens=[4583, 6464, 9257, 12075, 27214],
-    neg_tokens=[697, 1398, 3976, 5258, 9332, 14928],
-)
-
-
-get_scores_from_logits_falcon = partial(
-    get_scores_from_logits,
-    pos_tokens=[4879, 5007, 5159, 9109, 31792, 41489],
-    neg_tokens=[658, 1684, 2749, 2929, 9395, 10630],
-)
-
-get_scores_from_logits_mistral = partial(
-    get_scores_from_logits,
-    # 'Y', 'Yes', 'yes'
-    pos_tokens=[627, 5592, 5081],
-    # 'NO', 'No', 'no'
-    neg_tokens=[7929, 1770, 708],
-)
-
-def get_score_from_completion(choice):
-    p_yes, p_no, p_all = 0.0, 0.0, 0.0
-    for token, logprob in choice.logprobs.top_logprobs[0].items():
-        token = token.lower().lstrip()
-        prob = math.exp(logprob)
-        p_all += prob
-        if token.startswith("yes"):
-            p_yes += prob
-        elif token.startswith("no"):
-            p_no += prob
-    p_yes = p_yes if p_yes else 1 - p_all
-    p_no = p_no if p_no else 1 - p_all
-    if (p_yes <= 0) or (p_no <= 0):
-        return float("nan")
-    return math.log(p_yes) - math.log(p_no)
-
-
 def get_score_from_chat_completion(response, smoothing=1.0):
     texts = [choice.message.content.lower().lstrip() for choice in response.choices]
     n_yes, n_no = 0, 0
@@ -190,174 +61,6 @@ def get_score_from_chat_completion(response, smoothing=1.0):
             n_no += 1
     return math.log(n_yes + smoothing) - math.log(n_no + smoothing)
 
-
-@torch.no_grad()
-def generate_outputs(generator, inference_params, text, n_tokens, n=1, batch_size=1):
-    tokenizer, model = generator
-
-    inputs = tokenizer(
-        text,
-        return_tensors="pt",
-        padding=True,
-        truncation=True,
-        max_length=4096 - n_tokens,
-    ).to(model.device)
-
-    outputs = []
-    with ProgressBarStreamer(total=n_tokens * n) as pbar:
-        for i in range(0, n, batch_size):
-            n_batch = min(batch_size, n - i)
-            input_ids = inputs.input_ids.tile((n_batch, 1))
-            attention_mask = inputs.attention_mask.tile((n_batch, 1))
-            outputs_batch = model.generate(
-                input_ids,
-                attention_mask=attention_mask,
-                do_sample=True,
-                min_new_tokens=n_tokens,
-                max_new_tokens=n_tokens,
-                pad_token_id=tokenizer.eos_token_id,
-                streamer=pbar,
-                **inference_params
-            )
-            outputs.append(outputs_batch)
-
-    outputs = torch.cat(outputs)
-    out_texts = [tokenizer.decode(toks, skip_special_tokens=True) for toks in outputs]
-    in_length = len(tokenizer.decode(inputs.input_ids[0], skip_special_tokens=True))
-    return [out_texts[i][in_length:] for i in range(len(out_texts))]
-
-
-def generate_outputs_api(api_base, api_key, model_name, inference_params, text, n_tokens, n=1, verbose=False):
-    payload = {"n":n,
-               "max_tokens": n_tokens,
-               "model":model_name,
-               "prompt":text,
-               "stream":False,
-               **inference_params
-               }
-    if api_key is None:
-        header = {}
-    else:
-        header = {"Authorization": f"Bearer {api_key}"}
-    header["Content-Type"] = "application/json"
-    response = requests.post(api_base,
-                             headers=header,
-                             data=json.dumps(payload))
-    if verbose:
-        print("GEN API RESPONSE:", response.json())
-    texts = [choice["text"] for choice in response.json()["choices"]]
-    return texts
-
-
-def make_score_prompt_fn(evaluator, template, suffix, prompt, response):
-    tokenizer, model = evaluator
-    template_toks = tokenizer(template,
-                              return_tensors="pt",
-                              padding=True,
-                              truncation=True,
-                              max_length=4096)
-    template_length = len(template_toks.input_ids[0])
-    response_toks = tokenizer(response,
-                              return_tensors="pt",
-                              padding=True,
-                              truncation=True,
-                              max_length=4096 - template_length)
-    response_length = len(response_toks.input_ids[0])
-    prompt_toks = tokenizer(prompt,
-                            return_tensors="pt",
-                            padding=True,
-                            truncation=True,
-                            max_length=4096 - template_length - response_length)
-    response = tokenizer.decode(response_toks.input_ids[0], skip_special_tokens=True)
-    prompt = tokenizer.decode(prompt_toks.input_ids[0], skip_special_tokens=True)
-
-    return template.format(prompt = prompt, response = response) + suffix
-
-
-def make_score_prompt_api(template, suffix, prompt, response):
-    return template.format(prompt = prompt, response = response) + suffix
-
-
-@torch.no_grad()
-def evaluate_outputs(evaluator, score_prompt_fns, texts):
-    tokenizer, model = evaluator
-
-    if tokenizer.vocab["yes"] == 8505:
-        get_scores_from_logits = get_scores_from_logits_gpt2
-    elif tokenizer.vocab["yes"] == 9820:
-        get_scores_from_logits = get_scores_from_logits_neox
-    elif tokenizer.vocab["yes"] == 3582:
-        get_scores_from_logits = get_scores_from_logits_llama
-    elif tokenizer.vocab["yes"] == 9257:
-        get_scores_from_logits = get_scores_from_logits_openllama
-    elif tokenizer.vocab["yes"] == 9109:
-        get_scores_from_logits = get_scores_from_logits_falcon
-    elif tokenizer.vocab["yes"] == 9780:
-        get_scores_from_logits = get_scores_from_logits_mistral
-    else:
-        raise ValueError("Unknown model type")
-
-    scores = []
-    for score_prompt_fn in score_prompt_fns:
-        prompts = [score_prompt_fn(text[0], text[1]) for text in texts]
-        # TODO: currently, we take inference_params for evaluate_outputs_api, but not for evaluate_outputs
-        # is there a reason to prefer using forward() over generate() here?
-        tokens = tokenizer(
-            prompts,
-            return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=4096,
-        ).input_ids.to(model.device)
-        logits = model(tokens).logits
-        scores.append(
-            torch.tensor(
-                [score.item() for score in get_scores_from_logits(logits)]))
-    # TODO: Return these unpooled so the separate components can be stored in the
-    # weave tree
-    return torch.stack(scores).mean(dim=0)
-
-class Choice:
-    pass
-
-class MockLogProbs:
-    pass
-
-def evaluate_outputs_api(api_base, api_key, model_name, score_prompt_fns, inference_params, texts, n=1, verbose=False):
-    scores = []
-    for text in texts:
-        prompts = [score_prompt_fn(text[0], text) for score_prompt_fn in score_prompt_fns]
-#    for score_prompt_fn in score_prompt_fns:
-#        prompts = [score_prompt_fn(text) for text in texts]
-        payload = {"n":n,
-                   "max_tokens": 1,
-                   "model":model_name,
-                   "prompt":prompts,
-                   "stream":False,
-                   "logprobs":100,
-                   **inference_params
-                   }
-        if api_key is None:
-            header = {}
-        else:
-            header = {"Authorization": f"Bearer {api_key}"}
-        header["Content-Type"] = "application/json"
-        response = requests.post(api_base,
-                                 headers=header,
-                                 data=json.dumps(payload))
-        choices = []
-        if verbose:
-            print("EVAL API RESPONSE:", response.json())
-        for choice in response.json()["choices"]:
-            choice_o = Choice()
-            mocklogprobs_o = MockLogProbs()
-            choice_o.logprobs = mocklogprobs_o
-            choice_o.logprobs.top_logprobs = choice["logprobs"]["top_logprobs"]
-            choices.append(choice_o)
-        scores.append(torch.tensor([get_score_from_completion(choice) for choice in choices]))
-    # TODO: Return these unpooled so the separate components can be stored in the
-    # weave tree
-    return torch.stack(scores).mean(dim=1)
 
 class TreeNode:
     max_id = 0
@@ -553,32 +256,6 @@ def weave_tree_search(
     )
     return nodes[:beam_width]
 
-def make_gen_eval_fns(config, evaluation_prompt, verbose=False):
-    if config['generator']['api_base'] is None:
-        print("Loading generator model...")
-        generator = load_generator(config['generator']['model_name'], config['generator']['load_dtype'])
-        generate_fn = partial(generate_outputs, generator, config['generator']['inference_params'],
-                              batch_size=config['generator']['batch_size'])
-    else:
-        generate_fn = partial(generate_outputs_api, config['generator']['api_base'], config['generator']['api_key'],
-                              config['generator']['model_name'], config['generator']['inference_params'],
-                                verbose=verbose)
-
-    if config['evaluator']['api_base'] is None:
-        print("Loading evaluator model...")
-        evaluator = load_evaluator(config['evaluator']['model_name'], config['evaluator']['load_dtype'])
-        score_prompt_fn = partial(make_score_prompt_fn,
-                                  evaluator,
-                                  evaluation_prompt,
-                                  "<|end|>")
-        evaluate_fn = partial(evaluate_outputs, evaluator, [score_prompt_fn])
-    else:
-        score_prompt_fn = partial(make_score_prompt_api, evaluation_prompt, "<|end|>")
-        evaluate_fn = partial(evaluate_outputs_api, config['evaluator']['api_base'], config['evaluator']['api_key'],
-                              config['evaluator']['model_name'], [score_prompt_fn],
-                              config['evaluator']['inference_params'], verbose=verbose)
-    return generate_fn, evaluate_fn
-
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--config", "-c", help="Path to the configuration file", default="configs/gpt2.json")
@@ -591,7 +268,7 @@ def main():
 
     w_p = config['init_weave_param']
 
-    generate_fn, evaluate_fn = make_gen_eval_fns(config, w_p['evaluation_prompt'], verbose=args.verbose)
+    generate_fn, evaluate_fn = make_gen_eval_fns(config, w_p['evaluation_prompt'])
 
     system_prompt = ""
     prompt = "Once upon a time, there was a woman who"
